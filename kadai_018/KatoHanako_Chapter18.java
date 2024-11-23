8bf1bbbe4d33583898570b36ef218885b3fb4963
VD3baeKy <digfehc@gmail.com>
2024-11-23 19:23:23 +0900

    Update KatoHanako_Chapter18.java
    ・氏名を紹介するように修正。
    ・commonIntroduceで、共通の紹介が出力されるように修正。

--- kadai_018/KatoHanako_Chapter18.java
+++ kadai_018/KatoHanako_Chapter18.java
@@ -10,6 +10,8 @@ public class KatoHanako_Chapter18 extends Kato_Chapter18{ //子クラス
 	
 	// 【メソッド】
 	public void eachIntroduce() {  //抽象メソッドに具体的な処理を記述
-		System.out.println("趣味は読書です");	
+		System.out.println("名前は" + familyName + this.givenName + "です");
+		commonIntroduce();
+		System.out.println("趣味は読書です\n");	
 	}
 }
