a5d1961bc1153855ed912eb2172eadb81bfb64c8
VD3baeKy <digfehc@gmail.com>
2024-11-23 20:27:18 +0900

    Update KatoIchiro_Chapter18.java
    名前の出力も、commonIntroduceで行うように修正。

--- kadai_018/KatoIchiro_Chapter18.java
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -10,8 +10,8 @@ public class KatoIchiro_Chapter18 extends Kato_Chapter18{ //子クラス
 	
 	// 【メソッド】
 	public void eachIntroduce() {  //抽象メソッドに具体的な処理を記述
-		System.out.println("名前は" + familyName + this.givenName + "です");
-		commonIntroduce();
+		//System.out.println("名前は" + familyName + this.givenName + "です");
+		commonIntroduce(this.givenName);
 		System.out.println("好きな食べ物はリンゴです\n");
 	
 	}
