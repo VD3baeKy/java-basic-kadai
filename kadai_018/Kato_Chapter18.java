dc3b008a95e284ea5a08673ef19fbba6f88841a2
VD3baeKy <digfehc@gmail.com>
2024-11-23 19:26:53 +0900

    Update Kato_Chapter18.java
    ・姓と住所の値を設定。
    ・commonIntroduceで、共通の紹介が出力されるように修正。

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -4,13 +4,13 @@ package kadai_018;
 abstract public class Kato_Chapter18 { //親クラス
 	
 	//フィールド
-	public String familyName;	//姓を表す
-	public String givenName;	//名を表す
-	public String address;		//住所を表す
+	public String familyName = "加藤";	  //姓を表す
+	public String givenName;                  //名を表す
+	public String address = "東京都中野区〇×";  //住所を表す
 
 	// 【メソッド】
 	public void commonIntroduce() { //共通の紹介を出力する
-		
+	　　System.out.println("住所は" + this.address + "です");
 	}
 	
 	// 【抽象メソッド】
@@ -18,6 +18,6 @@ abstract public class Kato_Chapter18 { //親クラス
 	
 	// 【メソッド】
 	public void execIntroduce() { //紹介を実行する
-		this.eachIntroduce();	
+	　　this.eachIntroduce();
 	}
 }
